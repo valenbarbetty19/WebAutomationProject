d5c0bf458245464a50e0f14e21c4161ec11bf22e
valenbarbetty19 <valentina.barbetty@globant.com>
2026-03-20 09:49:12 -0500

    Logout Test working

--- src/test/java/selenium/pom/pages/InventoryPage.java
+++ src/test/java/selenium/pom/pages/InventoryPage.java
@@ -2,6 +2,8 @@ package selenium.pom.pages;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
 import java.util.Random;
@@ -13,7 +15,11 @@ public class InventoryPage extends BasePage {
 
     @FindBy(id = "shopping_cart_container")
     private WebElement cart;
+    @FindBy(id = "react-burger-menu-btn")
+    private WebElement menuBtn;
 
+    @FindBy(id = "logout_sidebar_link")
+    private WebElement logoutBtn;
     public InventoryPage(WebDriver driver){
         super(driver);
     }
@@ -30,5 +36,17 @@ public class InventoryPage extends BasePage {
             products.get(i).findElement(By.tagName("button")).click();
         }
     }
+    public LoginPage logout(){
+        menuBtn.click();
+
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.elementToBeClickable(logoutBtn));
+
+        logoutBtn.click();
+
+        wait.until(ExpectedConditions.urlContains("saucedemo"));
+        return new LoginPage(driver);
+    }
+
 
 }
